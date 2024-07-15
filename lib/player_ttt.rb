7640da0f0e1b232f70cd2ea16435ff9452e817d0
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-07-15 14:34:27 +0700

    feat: add player object

---
+++ lib/player_ttt.rb
@@ -0,0 +1,40 @@
+# Tic-tac-toe player object
+class PlayerTTT
+  def initialize(mark_type)
+    raise 'invalid mark type' if mark_type != 'X' || mark_type != 'O'
+
+    @mark_type = mark_type
+    @score = 0
+    @table = nil
+  end
+
+  def play_on(table)
+    @table = table
+  end
+
+  def play(position)
+    return false if @table.nil?
+
+    @table.add_mark(position, @mark_type)
+  end
+
+  def win?
+    marks = mark_type == 'X' ? @table.mark_x : @table.mark_o
+
+    possible_win_marks = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
+                          [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+    result = false
+
+    possible_win_marks.each do |win_marks|
+      result ||= include_marks?(marks, win_marks)
+    end
+
+    result
+  end
+
+  private
+
+  def include_marks?(marks, submarks)
+    (marks & submarks).eql?(submarks)
+  end
+end
