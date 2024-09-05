9e8032acf25448c02dfe8041ef0ae9ad1349b76f
Pinminh <minh.nguyenhongk22@hcmut.edu.vn>
2024-09-05 19:41:41 +0700

    feat: give more details in tests of PlayerTTT#win?

--- spec/player_ttt_spec.rb
+++ spec/player_ttt_spec.rb
@@ -1,51 +1,53 @@
 require_relative '../lib/player_ttt'
 
 describe PlayerTTT do
-  context 'when player is X' do
-    subject(:player_x) { described_class.new('X') }
-    let(:table_x) { double('table_ttt') }
+  describe '#win?' do
+    context 'when player is X' do
+      subject(:player_x) { described_class.new('X') }
+      let(:table_x) { double('table_ttt') }
 
-    context 'and there is a row of X on the table' do
-      before do
-        player_x.instance_variable_set(:@table, table_x)
-        allow(table_x).to receive(:mark_x).and_return([1, 2, 3, 4, 5, 7, 9])
+      context 'and there is a row of X on the table' do
+        before do
+          player_x.instance_variable_set(:@table, table_x)
+          allow(table_x).to receive(:mark_x).and_return([1, 2, 3, 4, 5, 7, 9])
+        end
+
+        it 'declares that X won' do
+          expect(player_x).to be_win
+        end
       end
 
-      it 'declares that X won' do
-        expect(player_x).to be_win
-      end
-    end
+      context 'and there is a column of X on the table' do
+        before do
+          player_x.instance_variable_set(:@table, table_x)
+          allow(table_x).to receive(:mark_x).and_return([3, 5, 6, 8, 9])
+        end
 
-    context 'and there is a column of X on the table' do
-      before do
-        player_x.instance_variable_set(:@table, table_x)
-        allow(table_x).to receive(:mark_x).and_return([3, 5, 6, 8, 9])
-      end
-
-      it 'declares that X won' do
-        expect(player_x).to be_win
-      end
-    end
-
-    context 'and there is a diagonal of X on the table' do
-      before do
-        player_x.instance_variable_set(:@table, table_x)
-        allow(table_x).to receive(:mark_x).and_return([2, 3, 5, 6, 7, 8])
+        it 'declares that X won' do
+          expect(player_x).to be_win
+        end
       end
 
-      it 'declares that X won' do
-        expect(player_x).to be_win
-      end
-    end
+      context 'and there is a diagonal of X on the table' do
+        before do
+          player_x.instance_variable_set(:@table, table_x)
+          allow(table_x).to receive(:mark_x).and_return([2, 3, 5, 6, 7, 8])
+        end
 
-    context 'and there is neither a row, column nor diagonal of X on the table' do
-      before do
-        player_x.instance_variable_set(:@table, table_x)
-        allow(table_x).to receive(:mark_x).and_return([3, 4, 5, 8, 9])
+        it 'declares that X won' do
+          expect(player_x).to be_win
+        end
       end
 
-      it 'declares that X has not yet won' do
-        expect(player_x).not_to be_win
+      context 'and there is neither a row, column nor diagonal of X on the table' do
+        before do
+          player_x.instance_variable_set(:@table, table_x)
+          allow(table_x).to receive(:mark_x).and_return([3, 4, 5, 8, 9])
+        end
+
+        it 'declares that X has not yet won' do
+          expect(player_x).not_to be_win
+        end
       end
     end
   end
